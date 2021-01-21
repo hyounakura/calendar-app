4fa69c5be4c21d655559a7c9e1468120e62ceea9
hyounakura <leopardtender@gmail.com>
2021-01-21 12:02:33 +0900

    displayCalendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
 
-const Calendar = () => {
-  return <div>hello</div>
-};
-
-export default Calendar;
+export default class Calendar extends React.Component {
+  render() {
+    return (
+      <FullCalendar
+        locale="ja"
+        plugins={[ dayGridPlugin, timeGridPlugin ]}
+        initialView="dayGridMonth"
+        headerToolbar={{ // カレンダーのヘッダー設定。(詳細は後述。※2)
+          start: 'prev, next, today',
+          center: 'title',
+          end: 'dayGridMonth,timeGridWeek'
+        }}
+      />
+    )
+  }
+}
